2b051fe28123503091ef09501d727a38afc333a6
markhoyt84 <mark.hoyt970@gmail.com>
2017-03-05 09:33:15 -0700

    fixed uri issues

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,7 @@ router.get('/', function(req, res, next) {
   async.parallel({
     bittrexData: function(callback) {
       request({
-        url: 'http://0.0.0.0:3000/api/getCurrentBittrexData',
+        url: 'https://ancient-brushlands-51832.herokuapp.com/api/getCurrentBittrexData',
         method: 'GET'
       }, function (err, response, body) {
         if(err) {
@@ -25,7 +25,7 @@ router.get('/', function(req, res, next) {
     },
     btceData: function(callback) {
       request({
-        url: 'http://0.0.0.0:3000/api/getCurrentBTCEData',
+        url: 'https://ancient-brushlands-51832.herokuapp.com/api/getCurrentBTCEData',
         method: 'GET'
       }, function (err, response, body) {
         if(err) {
