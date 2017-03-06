99f28b7f26687c9064768db024a930051f210a52
markhoyt84 <mark.hoyt970@gmail.com>
2017-03-06 08:39:39 -0700

    changed url to new app name and updated api calls accordingly

--- routes/conversions.js
+++ routes/conversions.js
@@ -15,7 +15,7 @@ router.convertCurrency = function (currencyType, amount, next) {
   async.parallel({
     bittrex: function(callback) {
       request({
-            url: 'https://ancient-brushlands-51832.herokuapp.com/api/getCurrentBittrexData',
+            url: 'http://shapeless-shifter.herokuapp.com/api/getCurrentBittrexData',
             method: 'GET',
             qs: {currencyType: currencyType},
           }, function (err, response, body) {
@@ -35,7 +35,7 @@ router.convertCurrency = function (currencyType, amount, next) {
     },
     btceData: function(callback) {
       request({
-        url: 'https://ancient-brushlands-51832.herokuapp.com/api/getCurrentBTCEData',
+        url: 'http://shapeless-shifter.herokuapp.com/api/getCurrentBTCEData',
         method: 'GET',
         qs: {currencyType: currencyType},
       }, function (err, response, body) {
